2461a005f02d5422f166e8b816b0a4e1d0b77dcf
Pavankumar  Hiremath <pavankumar.h@intimetec.com>
2025-10-14 17:00:34 +0530

    First committ

---
+++ src/JavaDataTypes.java
@@ -0,0 +1,13 @@
+public class JavaDataTypes {
+    public static  void main(String[] args)
+    {
+        int myNum = 5;               // Integer (whole number)
+        double myFloatNum = 5.99;    // Floating point number
+        char myLetter = 'D';         // Character  data type is used to store a single character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+        //System.out.println(myNum);
+        System.out.println(myFloatNum);
+    }
+}
---
+++ src/Variable_Types.java
@@ -0,0 +1,14 @@
+public class Variable_Types {
+    public static  void main(String[] args)
+    {
+       // Variables are containers for storing data values.
+        String name = "John";
+        System.out.println(name);
+
+      // declare variables as final when their values should never change
+        final int MINUTES_PER_HOUR = 60;
+        final int BIRTHYEAR = 1980;
+        System.out.println(MINUTES_PER_HOUR);
+
+    }
+}
